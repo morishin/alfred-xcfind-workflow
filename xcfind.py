5124499d08e1660daa18101432b0f8e88493b528
shintaro-morikawa <sntr92@gmail.com>
2016-07-08 18:07:11 +0900

    Update

--- xcfind.py
+++ xcfind.py
@@ -1,9 +1,33 @@
-#!/usr/bin/env python
-
+import os
 import subprocess
 import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+ '/enum')
+sys.path.append(os.path.dirname(os.path.abspath(__file__))+ '/workflow')
+
+from enum import Enum
 from workflow import Workflow
+
+
+class FileType(Enum):
+    xcodeproj = 'xcode-project_Icon.icns'
+    xcworkspace = 'workspace_Icon.icns'
+    playground = 'playground_Icon.icns'
+
+    def extension(self):
+        return self.name
+
+    def icon(self):
+        return self.value
+
+def search(query, file_type):
+    find_command = "mdfind \"kMDItemFSName == '{0}*.{1}'c\"".format(query, file_type.extension())
+    found_paths = subprocess.check_output(find_command, shell=True).decode("utf-8").rstrip().split("\n")
+    results = []
+    for path in found_paths:
+        filename = path.split("/")[-1]
+        results.append((filename, path, file_type.icon()))
+    return results
 
 
 def main(wf):
@@ -15,23 +39,12 @@ def main(wf):
     xcode_path = subprocess.check_output("xcode-select -print-path", shell=True).decode("utf-8").rstrip()
     xcode_resouce_path= "/".join(xcode_path.split("/")[:-1]) + "/Resources/"
 
-    find_xcworkspace = "mdfind \"kMDItemFSName == '{0}*.xcworkspace'c\"".format(query)
-    xcworkspace_files = subprocess.check_output(find_xcworkspace, shell=True).decode("utf-8").rstrip().split("\n")
-    xcworkspace_icon_path = xcode_resouce_path + 'workspace_Icon.icns'
-    for path in xcworkspace_files:
-        filename = path.split("/")[-1]
-        wf.add_item(filename, path, arg=path, valid=True, icon=xcworkspace_icon_path)
-
-    find_xcodeproj = "mdfind \"kMDItemFSName == '{0}*.xcodeproj'c\"".format(query)
-    xcodeproj_files = subprocess.check_output(find_xcodeproj, shell=True).decode("utf-8").rstrip().split("\n")
-    xcodeproj_icon_path = xcode_resouce_path + 'xcode-project_Icon.icns'
-    for path in xcodeproj_files:
-        filename = path.split("/")[-1]
-        wf.add_item(filename, path, arg=path, valid=True, icon=xcodeproj_icon_path)
+    for fileType in [FileType.xcworkspace, FileType.playground, FileType.xcodeproj]:
+        for (filename, path, icon) in search(query, fileType):
+            wf.add_item(filename, path, arg=path, valid=True, icon=xcode_resouce_path + icon)
 
     wf.send_feedback()
 
 if __name__ == '__main__':
     wf = Workflow()
     sys.exit(wf.run(main))
-
